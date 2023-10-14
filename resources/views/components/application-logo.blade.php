f443b3c67ef56dd9efb32f7af35652eca9545055
jose_dms <019001412@upsrj.edu.mx>
2023-10-13 18:05:24 -0600

    Archivo subido correctamente

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="https://upload.wikimedia.org/wikipedia/commons/0/00/PlayStation_logo.svg" alt="" width="50">
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+<img src="https://upload.wikimedia.org/wikipedia/commons/0/00/PlayStation_logo.svg" alt="" width="250">
+</a>
